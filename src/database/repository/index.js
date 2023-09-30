532bad84ff68624b9c1e0105c51e4961a0505942
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-30 19:19:45 +0530

    repo connections

---
+++ src/database/repository/index.js
@@ -0,0 +1,12 @@
+import connection from "../connection";
+import ProductRepository from "./product-repository";
+import CustomerRepository from "./customer-repository";
+import ShoppingRepository from "./shopping-repository";
+
+// database related modules
+exports = {
+  connection,
+  ProductRepository,
+  CustomerRepository,
+  ShoppingRepository,
+};
