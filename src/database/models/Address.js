f8c1f6305b4047278af885150522f6ba1399cfbe
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-30 19:12:45 +0530

    add models

---
+++ src/database/models/Address.js
@@ -0,0 +1,12 @@
+import { Schema as _Schema, model } from 'mongoose';
+
+const Schema = _Schema;
+
+const AddressSchema = new Schema({
+    street: String,
+    postalCode: String,
+    city: String,
+    country: String
+});
+
+export default  model('address', AddressSchema);
---
+++ src/database/models/index.js
@@ -0,0 +1,11 @@
+import Customer from "./Customer";
+import Product from "./Product";
+import Order from "./Order";
+import Address from "./Address";
+
+exports = {
+  Customer,
+  Product,
+  Order,
+  Address,
+};
