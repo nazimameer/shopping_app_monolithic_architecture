279165da950f6710e883a94d9a24a4c98381edf2
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-30 19:03:57 +0530

    setup config

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+import {config} from 'dotenv'
+
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+   config({ path: configFile });
+} else {
+    config();
+}
+
+module.exports = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+};
