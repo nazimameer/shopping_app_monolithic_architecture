7a66771599271b3636d8587b3f55b29175735a73
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-30 19:06:38 +0530

    add port

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 const expressApp = require('./express-app');
-
+const { PORT } = require('./config')
 const StartApp = async () => {
     const app = express();
 
     await expressApp(app);
 
-
+    app.listen(PORT, () => {
+        console.log(`listening to port ${PORT}`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    })
 }
 
 
